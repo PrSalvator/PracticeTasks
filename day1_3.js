74ba1be914e969ed9a059b679d3e5298e2dab062
PrSalvator <kirill.leono2016yandex.ru@gmail.com>
2024-06-25 15:43:56 +0700

    .

--- day1_3.js
+++
@@ -1,9 +0,0 @@
-const strArr = ['дим', 'манек', 'рота'];
-
-strArr.forEach(str => {
-    console.log( str + reverse(str.slice(0, -1)) );
-})
-
-function reverse(str){
-    return str.split('').reverse().join('');
-}
